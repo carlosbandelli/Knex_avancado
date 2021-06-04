4e388a8d6ed16ed4eeadcb12c017d9499124cb5f
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-06-04 18:14:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 var database = require("./database")
-var dados = [
-/* INSERT
-{
-    nome: "Call of Duty WarZone",
-    preço: 60.00
-},
+// var dados = [
+//  INSERT
+// {
+//     nome: "Call of Duty WarZone",
+//     preço: 60.00
+// },
 
-{
-    nome: "League Of Legends",
-    preço: 30.00
-},
+// {
+//     nome: "League Of Legends",
+//     preço: 30.00
+// },
 
-{
-    nome: "CyberPunk 2077",
-    preço: 270.00
-},
+// {
+//     nome: "CyberPunk 2077",
+//     preço: 270.00
+// },
+// ]
+// database.insert(dados).into("games").then(data=>{
+//     console.log(data)
+// }).catch(err=>{
+//     console.log(err)
+// })
 
-
-
-]
-database.insert(dados).into("games").then(data=>{
+//SELECT
+database.select(["id","preço"]).table("games").then(data=>{
     console.log(data)
 }).catch(err=>{
     console.log(err)
 })
-*/
 
-database.select().table("games").then(data => {
-    console.log(data)
+
+// NESTED QUERIES
+database.insert({nome:"Mists of noyah", preço:"25"}).into("games").then(data => {
+    database.select(["id","preço"]).table("games").then(data => {
+        console.log(data)
+    }).catch(err => {
+        console.log(err)
+    })
 }).catch(err => {
     console.log(err)
 })
