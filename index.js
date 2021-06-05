36abb0834e79220cf57fc074bbc5584a82679566
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-06-05 01:36:42 -0300

    chave estrageira

--- index.js
+++ index.js
@@ -30,12 +30,69 @@ var database = require("./database")
 // })
 
 // NESTED QUERIES
-database.insert({nome:"Mists of noyah", preco:"25"}).into("games").then(data => {
-    database.select(["id","preco"]).table("games").then(data => {
-        console.log(data)
-    }).catch(err => {
-        console.log(err)
-    })
-}).catch(err => {
-    console.log(err)
+// database.insert({nome:"Mists of noyah", preco:"25"}).into("games").then(data => {
+//     database.select(["id","preco"]).table("games").then(data => {
+//         console.log(data)
+//     }).catch(err => {
+//         console.log(err)
+//     })
+// }).catch(err => {
+//     console.log(err)
+// })
+
+
+// WHERE
+// database.select(["id","preco"])
+//     .whereRaw("nome = 'Mists of noyah' OR preco > 50")
+//      .table("games").then(data =>{
+//         console.log(data)
+//     }).catch(err =>{
+//         console.log(err)
+//     })
+
+// Digamos que voce quer fazer uma query(uma linguagem sql) que o Knex não te dá essa flexibilidade
+//então voce pode burlar desse maneira
+// database.raw("SELECT * FROM games").then(data => {
+//     console.log(data)
+// }).catch(err =>{
+//     console.log(err)
+// })
+
+// //DELETE
+// database.where({id:3}).delete().table("games").then(data=>{
+//     console.log(data)
+// }).catch(err =>{
+//     console.log(err)
+// })
+
+
+//UPDATE
+// database.where({id: 4}).update({preco: 40}).table("games").then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+//Order by
+
+// database.select().table("games").orderBy("nome","desc").then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log(err)
+// }) // decresente
+
+
+// database.select().table("games").orderBy("nome","asc").then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log(err)
+// }) // crescente
+
+database.insert({
+    nome: "Riot",
+    game_id: 2
+}).table("estudios").then(data => {
+console.log(data)
+}).catch(err =>{
+console.log(err)
 })
