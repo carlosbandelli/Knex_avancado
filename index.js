cf659e3c4a0681ff4dabd34f91641b4d6d178a26
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2021-06-08 22:27:05 -0300

    transações

--- index.js
+++ index.js
@@ -137,4 +137,69 @@ database.select(["games.*", "estudios.nome as estudio_nome"]).table("games").inn
     console.log(err)
 })
 */ 
+/* COM WHERE EM GAMES
+database.select([
+    "estudios.nome as estudios_nome",
+    "estudios.id as estudio_id",
+    "games.nome as games_nome",
+    "games.id as games_id",
+    "games.preco"
+]).table("games_estudios")
+.innerJoin("games", "games.id", "games_estudios.game_id")
+.innerJoin("estudios","estudios.id","games_estudios.estudio_id")
+.where("games.id", 2)
+.then(data =>{
+    console.log(data)
+} ).catch(err =>{
+    console.log(err)
+})*/
 
+/* COM WHERE EM ESTUDIOS 
+database.select([
+    "estudios.nome as estudios_nome",
+    "estudios.id as estudio_id",
+    "games.nome as games_nome",
+    "games.id as games_id",
+    "games.preco"
+]).table("games_estudios")
+.innerJoin("games", "games.id", "games_estudios.game_id")
+.innerJoin("estudios","estudios.id","games_estudios.estudio_id")
+.where("estudios.id", 1)
+.then(data =>{
+    console.log(data)
+} ).catch(err =>{
+    console.log(err)
+})*/
+
+/* SEM WHERE
+database.select([
+    "estudios.nome as estudios_nome",
+    "estudios.id as estudio_id",
+    "games.nome as games_nome",
+    "games.id as games_id",
+    "games.preco"
+]).table("games_estudios")
+.innerJoin("games", "games.id", "games_estudios.game_id")
+.innerJoin("estudios","estudios.id","games_estudios.estudio_id")
+.then(data =>{
+    console.log(data)
+} ).catch(err =>{
+    console.log(err)
+})*/
+
+
+async function testeTransacao(){
+    try{
+        await database.transaction(async trans => {
+            await database.insert({nome: "Qualquer nome"}).table("estudios");
+            await database.insert({nome: "Pyxerelia"}).table("estudios");
+            await database.insert({nome: "Mojang"}).table("estudios");
+            await database.insert({nome: "Gearbox"}).table("estudios")
+        })
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+testeTransacao()
